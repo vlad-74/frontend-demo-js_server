0e2ccfd55c275025e68f76bc70cace0479c5cd3f
Vladimi Kurguzov <kurguzov74@gmail.com>
2017-12-05 21:23:42 +0300

    404 commit

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ var express = require("express");
     res.sendfile('frontend/index.html')
  });
 
+  app.get("/test", function(req, res) {
+    res.end('res.end frontend/index.html');
+ });
+
+  //The 404 Route (ALWAYS Keep this as the last route)
+  app.get('*', function(req, res){
+    res.send('what???', 404);
+  });
+
+// app.use(function(req, res, next) {
+//   if (req.url == '/') {
+//     res.end("Hello");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use(function(req, res, next) {
+//   if (req.url == '/test') {
+//     res.end("Test");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use(function(req,res){
+//     res.render('404.jade');
+// });
+
  /* serves all the static files */
  app.get(/^(.+)$/, function(req, res){ 
      console.log('static file request : ' + req.params);
